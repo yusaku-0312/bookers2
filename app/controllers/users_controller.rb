448ac455d45c2296861655678aa3c67a9c5218a6
yusaku-0312 <161700710+yusaku-0312@users.noreply.github.com>
2024-09-22 14:41:00 +0000

    [Finish] ファイル設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,33 @@
 class UsersController < ApplicationController
+  
+  def new
+  end
+  
+  def create
+  end
+  
+  def index
+    @users = User.all
+    @user = current_user
+  end
+  
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
