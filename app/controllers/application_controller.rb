af5d25e88d6458670a490b208bf925ceacd11d59
yusaku-0312 <161700710+yusaku-0312@users.noreply.github.com>
2024-09-26 10:08:49 +0000

    [Finish]バリデーション

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,6 @@ class ApplicationController < ActionController::Base
   
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  def after_sign_up_path_for(resource)
-    flash[:notice] = "Signed in successfully"
-    user_path(current_user.id)
-  end
-  
   def after_sign_in_path_for(resource)
     if params[:controller] == "devise/registrations"
       flash[:notice] = "Welcome! You have signed up successfully."
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,34 +2,39 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def index
     @books = Book.all
     @user = current_user
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
-    @book.user_id = current_user.id  
+    @book.user_id = current_user.id
     if @book.save
       flash[:notice] = "You have created book successfully."
+      
       redirect_to book_path(@book.id)
     else
+      @user = @book.user
+      @books = Book.all
       render :index
     end
   end
-  
+
   def show
     @book = Book.find(params[:id])
     @user = @book.user
+    
     @new_book = Book.new
+
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
@@ -39,18 +44,18 @@ class BooksController < ApplicationController
       render :edit
     end
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
-  
+
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body, :user_id)
   end
-  
+
 end
