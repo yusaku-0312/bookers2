401ad899ed573b652b8e6cb4664a57372ef8d132
yusaku-0312 <161700710+yusaku-0312@users.noreply.github.com>
2024-10-11 07:45:11 +0000

    [Finish]いいね機能, ビューコードまだしてない

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,15 @@
 class Book < ApplicationRecord
   
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   belongs_to :user
   
   validates :title, presence: true
   validates :body, length: { in: 1..200 }
+  
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   
   has_many :books, dependent: :destroy
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   
   validates :name, length: { in: 2..20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
